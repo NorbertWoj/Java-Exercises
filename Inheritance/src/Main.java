bf7f2b5b5ed6c2acec239b2e62f46da059510ee8
Norbert <nwojcikrbert@gmail.com>
2023-02-27 18:40:08 +0100

    created a Worker class and an Employee class

--- Inheritance/src/Main.java
+++ Inheritance/src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+        Employee Adam = new Employee("Adam", "11/11/1985", "01/01/2025");
+        System.out.println(Adam);
+        System.out.println("Age = " + Adam.getAge());
+        System.out.println("Pay = " + Adam.collectPay());
+
+        Employee Joe = new Employee("Joe", "11/11/1990", "03/03/2020");
+        System.out.println(Joe);
 
     }
 }
