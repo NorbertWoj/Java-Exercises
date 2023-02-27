53eda3f1981bc37f2f89a49d1c7c6bc2d6d916dd
Norbert <nwojcikrbert@gmail.com>
2023-02-27 21:37:38 +0100

    added  HourlyEmployee  and SalariedEmployee classes

--- Inheritance/src/Main.java
+++ Inheritance/src/Main.java
@@ -1,12 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        Employee Adam = new Employee("Adam", "11/11/1985", "01/01/2025");
-        System.out.println(Adam);
-        System.out.println("Age = " + Adam.getAge());
-        System.out.println("Pay = " + Adam.collectPay());
+        Employee adam = new Employee("Adam", "11/11/1985", "01/01/2025");
+        System.out.println(adam);
+        System.out.println("Age = " + adam.getAge());
+        System.out.println("Pay = " + adam.collectPay());
 
-        Employee Joe = new Employee("Joe", "11/11/1990", "03/03/2020");
-        System.out.println(Joe);
+        SalariedEmployee joe = new SalariedEmployee("Joe", "11/11/1990", "03/03/2020",35000);
+        System.out.println(joe);
+        System.out.println("Joe's Paycheck = $" + joe.collectPay());
 
+        joe.retire();
+        System.out.println("Joe's Pension check = $" + joe.collectPay());
+
+        HourlyEmployee amber = new HourlyEmployee("Amber","15/10/1995","23/12/2025",15.85);
+        System.out.println(amber);
+        System.out.println("Amber's Paycheck = $" + amber.collectPay());
+        System.out.println("Amber's Holiday Pay = $" + amber.getDoublePay());
     }
 }
