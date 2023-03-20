88ccea651dd315b9037aa47e4acb22b33a41321b
Norbert <norbert.wojcik.it@gmail.com>
2023-03-20 14:50:55 +0100

    added MinimumElementChallenge

---
+++ MinimumElementChallenge/src/Main.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] returnedArray = readIntegers();
+        System.out.println(Arrays.toString(returnedArray));
+
+        int returnedMin = findMin(returnedArray);
+        System.out.println("Min = " + returnedMin);
+    }
+
+    private static int[] readIntegers() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a list of integers, separated by comas: ");
+        String input = scanner.nextLine();
+
+        String[] splits = input.split(",");
+        int[] values = new int[splits.length];
+
+        for (int i = 0; i < splits.length; i++) {
+            values[i] = Integer.parseInt(splits[i].trim());
+        }
+        return values;
+    }
+
+    private static int findMin(int[] array) {
+        int min = Integer.MAX_VALUE;
+        for (int element : array) {
+            if (element < min) {
+                min = element;
+            }
+        }
+        return min;
+    }
+}
