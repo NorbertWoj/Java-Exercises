0972c60dccb71cc0d1c3621858caa0d7256e97ec
Norbert <norbert.wojcik.it@gmail.com>
2023-03-20 09:09:18 +0100

    added BillsBurgerChallenge

---
+++ BillsBurgerChallenge/src/Main.java
@@ -0,0 +1,36 @@
+public class Main {
+    public static void main(String[] args) {
+//        Item coke = new Item("drink", "coke", 1.50);
+//        coke.printItem();
+//        coke.setSize("LARGE");
+//        coke.printItem();
+//
+//        Item avocado = new Item("Topping", "avocado", 1.50);
+//        avocado.printItem();
+//
+//        Burger burger = new Burger("regular", 4.00);
+//        burger.addToppings("BACON", "CHEESE", "MAYO");
+//
+//        burger.printItem();
+
+        MealOrder regularMeal = new MealOrder();
+        regularMeal.addBurgerToppings("BACON", "CHEESE", "MAYO");
+        regularMeal.setDrinkSize("LARGE");
+        regularMeal.printItemizedList();
+
+        System.out.println("\n\n");
+
+        MealOrder secondMeal = new MealOrder("turkey", "7-up", "chili");
+        secondMeal.addBurgerToppings("LETTUCE", "CHEESE", "MAYO");
+        secondMeal.setDrinkSize("SMALL");
+        secondMeal.printItemizedList();
+
+        System.out.println("\n\n");
+
+        MealOrder deluxeMeal = new MealOrder("deluxe", "7-up", "chili:");
+        deluxeMeal.addBurgerToppings("AVOCADO", "BACON", "LETTUCE",
+                "CHEESE", "MAYO");
+        deluxeMeal.setDrinkSize("SMALL");
+        deluxeMeal.printItemizedList();
+    }
+}
