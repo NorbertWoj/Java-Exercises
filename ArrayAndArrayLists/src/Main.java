4389fccf29df6e0aee2ee95b2dfee411cfdffc10
Norbert <norbert.wojcik.it@gmail.com>
2023-03-22 15:36:06 +0100

    added ArrayAndArrayLists

---
+++ ArrayAndArrayLists/src/Main.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String[] originalArray = new String[] {"First", "Second", "Third"};
+        var originalList = Arrays.asList(originalArray);
+
+        originalList.set(0, "one");
+        System.out.println("list: " + originalList);
+        System.out.println("array: " + Arrays.toString(originalArray));
+
+        originalList.sort(Comparator.naturalOrder());
+        System.out.println("array: " + Arrays.toString(originalArray));
+
+//        UnsupportedOperationException
+//        originalList.remove(0);
+//        originalList.add("fourth");
+
+        List<String> newList = Arrays.asList("Sunday", "Monday", "Tuesday");
+        System.out.println(newList);
+
+    }
+}
