de77aa0166628b46585bf1e01d8d02e1e300f1aa
Norbert <norbert.wojcik.it@gmail.com>
2023-03-22 14:20:58 +0100

    added ArrayLists

---
+++ ArrayLists/src/GroceryItem.java
@@ -0,0 +1,10 @@
+public record GroceryItem(String name, String type, int count) {
+    public GroceryItem (String name){
+        this(name,"DAIRY", 1);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%d %s in %s", count, name.toUpperCase(), type);
+    }
+}
---
+++ ArrayLists/src/Main.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        GroceryItem[] groceryArray = new GroceryItem[3];
+        groceryArray[0] = new GroceryItem("milk");
+        groceryArray[1] = new GroceryItem("apples", "PRODUCE", 6);
+        groceryArray[2] = new GroceryItem("oranges", "PRODUCE", 5);
+        System.out.println(Arrays.toString(groceryArray));
+
+        ArrayList objectList = new ArrayList();
+        objectList.add(new GroceryItem("Butter"));
+        objectList.add("Yogurt");
+
+        ArrayList<GroceryItem> groceryList = new ArrayList<>();
+        groceryList.add(new GroceryItem("Butter"));
+        groceryList.add(new GroceryItem("Milk"));
+        groceryList.add(new GroceryItem("Oranges", "PRODUCE", 5));
+        groceryList.set(0, new GroceryItem("Apples", "PRODUCE", 6));
+        groceryList.remove(1);
+        System.out.println(groceryList);
+
+    }
+}
