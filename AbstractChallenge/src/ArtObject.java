b0a2d63b342cb6460bfe0f05d5a3de0cdd9ac7ab
Norbert <norbert.wojcik.it@gmail.com>
2023-03-29 17:00:20 +0200

    added AbstractChallenge

---
+++ AbstractChallenge/src/ArtObject.java
@@ -0,0 +1,13 @@
+public class ArtObject extends ProductForSale {
+
+    public ArtObject(String type, double price, String description) {
+        super(type, price, description);
+    }
+
+    @Override
+    public void showDetails() {
+        System.out.println("This " + type + " is a beatiful reproduction");
+        System.out.printf("The price of the piece is $%6.2f %n", price);
+        System.out.println(description);
+    }
+}
---
+++ AbstractChallenge/src/OrderItem.java
@@ -0,0 +1,3 @@
+public record OrderItem(int qty, ProductForSale product) {
+
+}
